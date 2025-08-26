b8f7f682df39965cb9162675ada820522ca691fd
szwanne <103660814+szwanne@users.noreply.github.com>
2025-08-26 13:45:51 +0200

    Fix ALLOWED_HOSTS for Heroku

--- tripapp/backend/backend/settings.py
+++ tripapp/backend/backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 
-ALLOWED_HOSTS = ['trip-planner.herokuapp.com']
+ALLOWED_HOSTS = ['trip-planner-81608bb83021.herokuapp.com']
 
 
 # Application definition
